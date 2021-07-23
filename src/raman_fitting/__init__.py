cf51b2ed579ccd577b230642ffbb41ffd1acce10
MyPyDavid <wdz.wallace@protonmail.com>
2021-07-23 12:57:32 +0200

    bump version patch 0.6.14 because of pre-commit interference

--- src/raman_fitting/__init__.py
+++ src/raman_fitting/__init__.py
@@ -15,7 +15,7 @@ try:
 
     __version__ = version
 except:
-    __version__ = "__version__ = '0.6.12'"
+    __version__ = "__version__ = '0.6.14'"
 
 
 from raman_fitting.config import filepath_settings
