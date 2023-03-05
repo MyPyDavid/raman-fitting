33f2126c7781234086336ba62b6a65aa615c053b
David Wallace <david.wallace@tu-darmstadt.de>
2023-03-05 15:53:29 +0100

    fix dataclass default ovv

--- src/raman_fitting/processing/spectrum_constructor.py
+++ src/raman_fitting/processing/spectrum_constructor.py
@@ -49,7 +49,7 @@ class SpectrumDataLoader:
     # intensity: np.array = field(default=np.array([]), init=False)
     spectrum_length: int = field(default=0, init=False)
     info: Dict = field(default_factory=dict, repr=False)
-    ovv: pd.DataFrame = field(default=pd.DataFrame(), repr=False)
+    ovv: pd.DataFrame = field(default_factory=pd.DataFrame(), repr=False)
     run_kwargs: Dict = field(default_factory=dict, repr=False)
 
     def __post_init__(self):
