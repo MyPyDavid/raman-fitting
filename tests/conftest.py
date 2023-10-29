9e6faef85f901b5248ea6a5273bd740ce70187c9
David Wallace <wdz.wallace@protonmail.com>
2023-10-29 10:49:59 +0100

    test: mv example files to test_fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,19 +4,8 @@
 """
 Configuration file for pytest and commonly used fixtures
 """
-import pprint
 import sys
 import pathlib
 
-# Need this for local editable install pytest run to work
-# This pythonpath = "src" should have fixed it.
-if "src" not in sys.path:
-    sys.path.append("src")
-    print("added src to sys.path")
-    pprint.pprint(sys.path)
-    pprint.pprint(pathlib.Path.cwd())
-
-
-import raman_fitting
-
 # Global fixtures
+import pytest
