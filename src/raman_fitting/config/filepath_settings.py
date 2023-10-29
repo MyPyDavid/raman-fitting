fae205d4ab6fbcf9e9120146748622f3fd4ca2b6
David Wallace <wdz.wallace@protonmail.com>
2023-10-29 13:46:37 +0100

    chore: add config file option

--- src/raman_fitting/config/filepath_settings.py
+++ src/raman_fitting/config/filepath_settings.py
@@ -1,37 +1,36 @@
-# from .logging_config import get_console_handler
+from dataclasses import dataclass
 import logging
 from pathlib import Path
 import tempfile
 
-from raman_fitting import __package_name__
+
+import raman_fitting
+
 
 logger = logging.getLogger(__name__)
-
-from dataclasses import dataclass
-
 
 TEMP_DIR = tempfile.TemporaryDirectory()
 
 
 @dataclass
 class Config:
-    CONFIG_FILE = Path(__file__).resolve()
-    PACKAGE_ROOT = CONFIG_FILE.parent.parent
-    MODEL_DIR = PACKAGE_ROOT / "deconvolution_models"
-    TEST_FIXTURES = PACKAGE_ROOT / "test_fixtures"
+    CONFIG_FILE: Path = Path(__file__).resolve()
+    PACKAGE_ROOT: Path = CONFIG_FILE.parent.parent
+    MODEL_DIR: Path = PACKAGE_ROOT / "deconvolution_models"
+    TEST_FIXTURES: Path = PACKAGE_ROOT / "test_fixtures"
 
     # Home dir from pathlib.Path for storing the results
-    USER_PACKAGE_HOME = (
-        Path.home() / f".{__package_name__}"
+    USER_PACKAGE_HOME: Path = (
+        Path.home() / f".{raman_fitting.__package_name__}"
     )  # pyramdeconv is the new version package name
 
     # Optional local configuration file
-    LOCAL_CONFIG_FILE = USER_PACKAGE_HOME / "local_config.py"
+    LOCAL_CONFIG_FILE: Path = USER_PACKAGE_HOME / "local_config.py"
 
     # Storage file of the index
-    INDEX_FILE_NAME = f"{__package_name__}_index.csv"
+    INDEX_FILE_NAME: str = f"{raman_fitting.__package_name__}_index.csv"
 
-    TEST_RESULTS_DIR = Path(TEMP_DIR.name)
+    TEST_RESULTS_DIR: Path = Path(TEMP_DIR.name)
 
     CONFIG = {
         "defaults": {
@@ -40,19 +39,21 @@ class Config:
             "MODEL_DIR": MODEL_DIR,
             "USER_PACKAGE_HOME": USER_PACKAGE_HOME,
             "INDEX_FILE_NAME": INDEX_FILE_NAME,
-            "LOCAL_CONFIG_FILE": LOCAL_CONFIG_FILE,
         },
         "testing": {
             "RESULTS_DIR": TEST_RESULTS_DIR,
             "DATASET_DIR": TEST_FIXTURES,
+            "USER_CONFIG_FILE": TEST_FIXTURES / "raman_fitting.toml",
         },
         "make_examples": {
             "RESULTS_DIR": USER_PACKAGE_HOME / "make_examples",
             "DATASET_DIR": TEST_FIXTURES,
+            "USER_CONFIG_FILE": TEST_FIXTURES / "raman_fitting.toml",
         },
         "normal": {
             "RESULTS_DIR": USER_PACKAGE_HOME / "results",
             "DATASET_DIR": USER_PACKAGE_HOME / "datafiles",
+            "USER_CONFIG_FILE": USER_PACKAGE_HOME / "raman_fitting.toml",
         },
     }
     CONFIG['debug'] = CONFIG['testing']
