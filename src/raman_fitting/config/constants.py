8223e5322e321eddb6aecf068ea32a91db3d7525
David Wallace <wdz.wallace@protonmail.com>
2023-10-29 10:45:47 +0100

    chore: refactor config

--- src/raman_fitting/config/constants.py
+++
@@ -1,5 +0,0 @@
-"""
-Created on Tue Jun 22 10:36:02 2021
-
-@author: DW
-"""
--- src/raman_fitting/config/filepath_settings.py
+++ src/raman_fitting/config/filepath_settings.py
@@ -1,62 +1,58 @@
 # from .logging_config import get_console_handler
 import logging
-import pathlib
-from sys import exit
+from pathlib import Path
+import tempfile
 
 from raman_fitting import __package_name__
 
 logger = logging.getLogger(__name__)
 
-
-# import pandas as pd
-# pd.options.display.max_rows = 10
-# pd.options.display.max_columns = 10
-# __package_name__ = 'raman_fitting'
-
-CONFIG_FILE = pathlib.Path(__file__).resolve()
-PACKAGE_ROOT = CONFIG_FILE.parent.parent
-MODEL_DIR = PACKAGE_ROOT / "deconvolution_models"
-
-# TESTS_ROOT_DIR = PACKAGE_ROOT.parent.parent / "tests"
-# TESTS_ROOT_DIR =
-TESTS_DATASET_DIR = PACKAGE_ROOT / "datafiles" / "example_files"
-
-# Home dir from pathlib.Path for storing the results
-PACKAGE_HOME = (
-    pathlib.Path.home() / f".{__package_name__}"
-)  # pyramdeconv is the new version package name
+from dataclasses import dataclass
 
 
-try:
-    if not PACKAGE_HOME.is_dir():
-        try:
-            logger.warning(
-                f"Package home directory did not exist, will now be created at:\n{PACKAGE_HOME}\n--------------------"
-            )
-            PACKAGE_HOME.mkdir()
-        except Exception as exc:
-            logger.warning(
-                f"Package home mkdir unexpected error\n{exc}.\nFolder{PACKAGE_HOME} could not be created, exiting."
-            )
-            exit()
-    else:
-        logger.info(
-            f"Package home directory exists at:\n{PACKAGE_HOME}\n--------------------"
-        )
-except Exception as exc:
-    logger.warning(
-        f"Unexpected error with checking for package home folder:\nFolder:{PACKAGE_HOME}\nError:\n{exc}\n {__package_name__} can not run."
-    )
-    exit()
+TEMP_DIR = tempfile.TemporaryDirectory()
 
 
-TESTS_RESULTS_DIR = PACKAGE_HOME / "test_results"
+@dataclass
+class Config:
+    CONFIG_FILE = Path(__file__).resolve()
+    PACKAGE_ROOT = CONFIG_FILE.parent.parent
+    MODEL_DIR = PACKAGE_ROOT / "deconvolution_models"
+    TEST_FIXTURES = PACKAGE_ROOT / "test_fixtures"
 
-DATASET_DIR = PACKAGE_HOME / "datafiles"
-RESULTS_DIR = PACKAGE_HOME / "results"
+    # Home dir from pathlib.Path for storing the results
+    USER_PACKAGE_HOME = (
+        Path.home() / f".{__package_name__}"
+    )  # pyramdeconv is the new version package name
 
-# Optional local configuration file
-LOCAL_CONFIG_FILE = PACKAGE_HOME / "local_config.py"
+    # Optional local configuration file
+    LOCAL_CONFIG_FILE = USER_PACKAGE_HOME / "local_config.py"
 
-# Storage file of the index
-INDEX_FILE_NAME = f"{__package_name__}_index.csv"
+    # Storage file of the index
+    INDEX_FILE_NAME = f"{__package_name__}_index.csv"
+
+    TEST_RESULTS_DIR = Path(TEMP_DIR.name)
+
+    CONFIG = {
+        "defaults": {
+            "CONFIG_FILE": CONFIG_FILE,
+            "PACKAGE_ROOT": PACKAGE_ROOT,
+            "MODEL_DIR": MODEL_DIR,
+            "USER_PACKAGE_HOME": USER_PACKAGE_HOME,
+            "INDEX_FILE_NAME": INDEX_FILE_NAME,
+            "LOCAL_CONFIG_FILE": LOCAL_CONFIG_FILE,
+        },
+        "testing": {
+            "RESULTS_DIR": TEST_RESULTS_DIR,
+            "DATASET_DIR": TEST_FIXTURES,
+        },
+        "make_examples": {
+            "RESULTS_DIR": USER_PACKAGE_HOME / "make_examples",
+            "DATASET_DIR": TEST_FIXTURES,
+        },
+        "normal": {
+            "RESULTS_DIR": USER_PACKAGE_HOME / "results",
+            "DATASET_DIR": USER_PACKAGE_HOME / "datafiles",
+        },
+    }
+    CONFIG['debug'] = CONFIG['testing']
