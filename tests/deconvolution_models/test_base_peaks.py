a317656241aae126e9947b0dc3c3fb83693b0fc5
David Wallace <wdz.wallace@protonmail.com>
2023-11-25 11:57:25 +0100

    tests: update tests

--- tests/deconvolution_models/test_base_peaks.py
+++ tests/deconvolution_models/test_base_peaks.py
@@ -5,19 +5,16 @@ import unittest
 import pytest
 from lmfit import Model
 
-# from raman_fitting.deconvolution_models import first_order_peaks
-# import raman_fitting
-from raman_fitting.deconvolution_models.default_peaks.base_peak import (
+
+from raman_fitting.deconvolution_models.base_peak import (
     BasePeak,
     BasePeakWarning,
-    LMfitModelConstructorMethods,
 )
+
+from pydantic import ValidationError
 
 logger = logging.getLogger(__name__)
 logging.captureWarnings(True)  # sends these warning to the logger
-
-
-# %%
 
 
 def _error_message_contains(excinfo, testmsg: str, verbose: bool = False):
@@ -39,325 +36,142 @@ def _error_message_contains(excinfo, testmsg: str, verbose: bool = False):
     return _test
 
 
-# %%
-
-
 class TestBasePeak(unittest.TestCase):
-    # %% TESTING
+    def test_basepeak_initialization(self):
+        # self.assertRaises(ValidationError, BasePeak())
+        # self.assertRaises(ValidationError, BasePeak(), peak_name="test")
+        # self.assertRaises(ValidationError, BasePeak(), peak_type="Voigt")
 
-    def test_BasePeak_attributes(self):
-        self.assertTrue(BasePeak.__doc__)
-        self.assertTrue(BasePeak._fields)
-        self.assertTrue(BasePeak._sources)
-        self.assertTrue(BasePeak.PEAK_TYPE_OPTIONS)
+        test_peak = BasePeak(peak_name="test", peak_type="Voigt")
+        assert test_peak.peak_name == "test"
 
-    def test_empty_base_class_raises(self):
-        # %%
-
-        class EmptyTestChild(metaclass=BasePeak):
-            pass
-
-        eb = EmptyTestChild()
-
-        with pytest.raises(ValueError) as excinfo:
-            eb.peak_type = "emptytest"
-        assert _error_message_contains(excinfo, "value emptytest for peak_type not in")
-
-        with pytest.raises(ValueError) as excinfo:
-            eb.peak_name = 10 * "emptytest"
-        assert _error_message_contains(excinfo, "value for peak_name is too long 90")
-
-        self.assertFalse(eb.peak_model)
-
-        # %%
-
+    @unittest.skip("TODO: add field validations")
     def test_empty_base_class_with_kwargs_raises(self):
-        # %%
-
-        class EmptyTestChild(
-            metaclass=BasePeak,
-            testkwarg2=2,
-            testkwarg3=3,
-            peak_type="Voigt",
-            verbose=True,
-        ):
-            pass
-
-        eb = EmptyTestChild()
-        # %%
+        eb = BasePeak(peak_type="Voigt", peak_name="test")
 
         self.assertEqual(eb.peak_type, "Voigt")
-        self.assertEqual(eb.testkwarg2, 2)
 
-        with pytest.raises(ValueError) as excinfo:
-            eb.peak_type = "emptytest"
-        self.assertTrue(
-            _error_message_contains(excinfo, "value emptytest for peak_type not in")
-        )
-
-        with pytest.raises(ValueError) as excinfo:
-            eb.peak_name = 10 * "emptytest"
-        self.assertTrue(
-            _error_message_contains(excinfo, "value for peak_name is too long 90")
-        )
-
-        self.assertFalse(eb.peak_model)
-        # with pytest.raises(AttributeError) as excinfo:
-        #     eb.peak_model
+        # TODO built in field validation str_length
+        # with pytest.raises(ValueError) as excinfo:
+        #     eb.peak_name = 10 * "emptytest"
         # self.assertTrue(
-        #     _error_message_contains(
-        #         excinfo, "type object empty no attribute _peak_model"
-        #     )
+        #     _error_message_contains(excinfo, "value for peak_name is too long 90")
         # )
 
-        with pytest.raises(ValueError) as excinfo:
-            eb.peak_type = "VoigtLorentzian"
-        self.assertTrue(
-            _error_message_contains(
-                excinfo,
-                ''''Multiple options ['Lorentzian', 'Voigt'] for misspelled value "VoigtLorentzian"''',
-            )
-        )
-
-    def test_empty_base_class_with_false_input(self):
-        # %%
-
-        class EmptyTestChild(metaclass=BasePeak, peak_type="FalsePeak"):
-            pass
-
-        with pytest.raises(ValueError) as excinfo:
-            eb = EmptyTestChild()
-        self.assertTrue(
-            _error_message_contains(excinfo, "value emptytest for peak_type not in")
-        )
-
-    # %%
-    def test_base_class_good_with_init_extra_tests(self):
-        # %%
-        class TestD1peak(metaclass=BasePeak, debug=True):
-            """
-            here is docstring of TestD1peak,
-            small spelling error on peak_type
-            """
-
-            def __init__(self, *args, **kwargs):
-                print(f"called __init__ {self} TestD1peak, with {args}, {kwargs}")
-                self._peak_type = "Voigt"
-                self.peak_name = "D1D1"
-                self.input_param_settings = {
-                    "center": {"value": 2650, "min": 2600, "max": 2750},
-                    "sigma": {"value": 60, "min": 1, "max": 200},
-                    "amplitude": {"value": 14, "min": 1e-03, "max": 100},
-                }
-
-        td1 = TestD1peak()
-        peakmod = "<lmfit.Model: Model(voigt, prefix='D1D1_')>"
-        self.assertEqual(str(td1.peak_model), peakmod)
-        _class_str = f"TestD1peak, {peakmod}, center : 2600 < 2650 > 2750"
-        self.assertIn(_class_str, str(td1))
-        td1.peak_name = "R2D2"
-        self.assertEqual(td1.peak_model.prefix, "R2D2_")
-
-        # _def_param = td1.param_hints_constructor({})
-        # _def_key = list(BasePeak.default_settings.keys())[0]
-        # _def_param[_def_key].value == BasePeak.default_settings[_def_key]["value"]
-
-        # _def_param = td1.param_hints_constructor(
-        #     td1.fco.register["init"]["param_hints"]
-        # )
-        # self.assertEqual(_def_param["amplitude"].value, 14)
-
-        # with pytest.raises(TypeError) as excinfo:
-        #     _def_param = td1.param_hints_constructor("fail")
+        # TODO built in field validation for peak_type
+        # with pytest.raises(ValueError) as excinfo:
+        #     eb.peak_type = "VoigtLorentzian"
         # self.assertTrue(
         #     _error_message_contains(
         #         excinfo,
-        #         "input_param_hints should be of type dictionary not <class 'str'>",
+        #         ''''Multiple options ['Lorentzian', 'Voigt'] for misspelled value "VoigtLorentzian"''',
         #     )
         # )
 
-        # _err_hints = copy.copy(td1.fco.register["init"]["param_hints"])
-        # _err_hints["center"] = (1, 2, 3, 4)
+    def test_base_class_good_with_init_extra_tests(self):
+        td1_kwargs = dict(
+            peak_type="Voigt",
+            peak_name="D1D1",
+            param_hints={
+                "center": {"value": 2650, "min": 2600, "max": 2750},
+                "sigma": {"value": 60, "min": 1, "max": 200},
+                "amplitude": {"value": 14, "min": 1e-03, "max": 100},
+            },
+        )
 
-        # with pytest.raises(ValueError) as excinfo:
-        #     _def_param = td1.param_hints_constructor(_err_hints)
-        # self.assertTrue(
-        #     _error_message_contains(
-        #         excinfo, " Unable to create a Parameter from center and (1, 2, 3, 4):"
-        #     )
-        # )
+        td1 = BasePeak(**td1_kwargs)
+        self.assertEqual(td1.peak_type, "Voigt")
+        self.assertEqual(td1.peak_name, "D1D1")
+        peakmod = "<lmfit.Model: Model(voigt, prefix='D1D1_')>"
+        self.assertEqual(str(td1.lmfit_model), peakmod)
+        # _class_str = f"center : 2600 < 2650 > 2750"
+        # self.assertIn(_class_str, str(td1))
+        # dont test attr setters
+        # td1.peak_name = "R2D2"
+        # self.assertEqual(td1.lmfit_model.prefix, "R2D2_")
 
-    # %%
     def test_base_class_good_with_init(self):
-        class TestD1peak(metaclass=BasePeak, debug=True):
-            """
-            test_base_class_good_with_init
-            but with spelling error in peak_type
-            """
+        d1_kwargs = dict(
+            peak_name="D1D1",
+            peak_type="Gaussian",
+            param_hints={
+                "center": {"value": 2650, "min": 2600, "max": 2750},
+                "sigma": {"value": 60, "min": 1, "max": 200},
+                "amplitude": {"value": 14, "min": 1e-03, "max": 100},
+            },
+        )
 
-            def __init__(self, *args, **kwargs):
-                print(f"called __init__ {self} TestD1peak, with {args}, {kwargs}")
-                self._peak_type = "cartVoigt"
-                self.peak_name = "D1D1"
-                self.input_param_settings = {
-                    "center": {"value": 2650, "min": 2600, "max": 2750},
-                    "sigma": {"value": 60, "min": 1, "max": 200},
-                    "amplitude": {"value": 14, "min": 1e-03, "max": 100},
-                }
-
-        td1 = TestD1peak()
-        _class_str = "TestD1peak, <lmfit.Model: Model(voigt, prefix='D1D1_')>, center : 2600 < 2650 > 2750"
-        self.assertIn(_class_str, str(td1))
-        # print(td1)
-
-    # %%
+        td1 = BasePeak(**d1_kwargs)
+        self.assertEqual(td1.peak_name, d1_kwargs["peak_name"])
 
     def test_base_class_good_with_init_added_method(self):
-        # %%
-        class TestD1peakmeta(metaclass=BasePeak, a=2):
-            """
-            here is docstring of TestD1peak
-            """
+        tkwargs = dict(
+            peak_type="Lorentzian",
+            peak_name="D1D1",
+            param_hints={
+                "center": {"value": 2650, "min": 2600, "max": 2750},
+                "sigma": {"value": 60, "min": 1, "max": 200},
+                "amplitude": {"value": 14, "min": 1e-03, "max": 100},
+            },
+        )
 
-            def __init__(self, *args, **kwargs):
-                # super().__init__(self)
-                # print(f'__subclass __init__ self "{self}" TestD1peakmeta, with {args}, {kwargs}')
-                self.peak_type = "Lorentzian"
-                self.peak_name = "D1D1"
-                self.input_param_settings = {
-                    "center": {"value": 2650, "min": 2600, "max": 2750},
-                    "sigma": {"value": 60, "min": 1, "max": 200},
-                    "amplitude": {"value": 14, "min": 1e-03, "max": 100},
-                }
-                self._meta_added_method(kwargs)
-
-            def _meta_added_method(self, *args, **kwargs):
-                """added method"""
-                print(f"{self.__dict__}")
-                self._added_method_arg = kwargs
-                print(f"added method called {kwargs}")
-                # return arg
-
-        td1m = TestD1peakmeta(add=33)
-        _teststr = "TestD1peakmeta, <lmfit.Model: Model(lorentzian, prefix='D1D1_')>, center : 2600 < 2650 > 2750"
-        self.assertIn(_teststr, str(td1m))
-        # assert str(td1m) == _teststr
-
-        # %%
+        td1m = BasePeak(**tkwargs)
+        # breakpoint()
+        self.assertEqual(td1m.peak_type, tkwargs["peak_type"])
+        self.assertEqual(td1m.peak_name, tkwargs["peak_name"])
 
     def test_base_class_good_with_attributes_and_init(self):
-        # %%
-        class NewChildClassAttr(metaclass=BasePeak):
-            """New child class for easier definition"""
-
-            _test = "testkwarg"
-
-            param_hints = {
+        tkwargs = dict(
+            param_hints={
                 "center": {"value": 2435, "min": 2400, "max": 2550},
                 "sigma": {"value": 30, "min": 1, "max": 200},
                 "amplitude": {"value": 2, "min": 1e-03, "max": 100},
-            }
-            peak_type = "Voigt"  #'Voigt'
-            peak_name = "R2D2"
+            },
+            peak_type="Voigt",
+            peak_name="R2D2",
+        )
 
-            def __init__(self, **kwargs):
-                # print(f'__init child empty pass {self}')
-                pass
-                # super().__init__()
-
-        nca = NewChildClassAttr()
-        _center_value = nca.peak_model.param_hints["center"]["value"]
+        nca = BasePeak(**tkwargs)
+        _center_value = nca.lmfit_model.param_hints["center"]["value"]
         assert _center_value == 2435
-        # print('Instance child:', nca)
-        # %%
 
     def test_base_class_good_with_attributes_no_init(self):
-        class NewChildClassAttrNoInit(metaclass=BasePeak):
-            """New child class for easier definition"""
-
-            _test = "testkwarg"
-
-            param_hints = {
+        tkwargs = dict(
+            param_hints={
                 "center": {"value": 2435, "min": 2400, "max": 2550},
                 "sigma": {"value": 30, "min": 1, "max": 200},
                 "amplitude": {"value": 2, "min": 1e-03, "max": 100},
-            }
-            peak_type = "Voigt"  #'Voigt'
-            peak_name = "R2D2"
-            # def __init__(self,**kwargs):
-            # print(f'__init child empty pass {self}')
-            # pass
-            # super().__init__()
+            },
+            peak_type="Voigt",
+            peak_name="R2D2",
+        )
 
-        ncni = NewChildClassAttrNoInit()
-        _center_value = ncni.peak_model.param_hints["center"]["value"]
-        assert _center_value == 2435
-
-        # %%
+        ncni = BasePeak(**tkwargs)
+        # breakpoint()
+        # _center_value =
+        assert ncni.param_hints["center"].value == 2435
+        assert ncni.lmfit_model.param_hints["center"]["value"] == 2435
 
     def test_base_class_good_with_attributes_init_collision_values(self):
-        # %%
-        class NewChildClassInit(metaclass=BasePeak, peak_type="Gaussian"):
-            """New child class for easier definition"""
-
-            _test = "testkwarg"
-            peak_type = "Lorentzian"
-            param_hints = {
+        tkwargs = dict(
+            param_hints={
                 "center": {"value": 2435, "min": 2400, "max": 2550},
                 "sigma": {"value": 30, "min": 1, "max": 200},
                 "amplitude": {"value": 2, "min": 1e-03, "max": 100},
-            }
-
-            def __init__(self, **kwargs):
-                self.peak_type = "Voigt"
-                self.peak_name = "R2D2"
-                self.param_hints = {"center": {"value": 20}}
-
-                # self.param_hints = {
-                #                     'center':
-                #                         {'value': 2435,'min': 2400, 'max': 2550},
-                #                     'sigma':
-                #                         {'value': 30,'min' : 1, 'max': 200},
-                #                     'amplitude' :
-                #                         {'value': 2,'min' : 1E-03, 'max': 100}
-                #                         }
-                # super().__init__(self)
-
-        nci = NewChildClassInit()
-        self.assertEqual(nci.peak_type, "Gaussian")
-        self.assertEqual(nci.peak_model.param_hints["center"]["value"], 2435)
-        # print(nci)
-        # print(self.__dict__)
-
-    def test_base_with_only_keyword_args(self):
-        new = BasePeak("newPeak", **{"noname": 2, "debug": False, "peak_type": "Voigt"})
-        new.param_hints = {"center": {"value": 200}}
-        newinst = new()
-        _newinst_str = "newPeak, <lmfit.Model: Model(voigt, prefix='newPeak_')>, center : -inf < 200 > inf"
-        self.assertEqual(str(newinst), _newinst_str)
-
-
-class TestLMfitModelConstructorMethods(unittest.TestCase):
-    LMfit = LMfitModelConstructorMethods
-
-    def test_make_model_from_peak_type_and_name(self):
-        model = self.LMfit.make_model_from_peak_type_and_name(
-            peak_type="Voigt", peak_name="lmfitpeak"
+            },
+            peak_type="Voigt",
+            peak_name="R2D2",
         )
-        self.assertTrue(isinstance(model, Model))
-        self.assertEqual(model.prefix, "lmfitpeak")
+        nci = BasePeak(**tkwargs)
+        self.assertEqual(nci.peak_type, "Voigt")
+        self.assertEqual(nci.lmfit_model.param_hints["center"]["value"], 2435)
 
-        with pytest.raises(NotImplementedError) as excinfo:
-            model = self.LMfit.make_model_from_peak_type_and_name(peak_type="FalsePeak")
-        self.assertTrue(
-            _error_message_contains(
-                excinfo,
-                " This peak type or model 'FalsePeak' has not been implemented.",
-            )
-        )
-
-
-# self = TestLMfitModelConstructorMethods()
+    # def test_base_with_only_keyword_args(self):
+    #     new = BasePeak("newPeak", **{"noname": 2, "debug": False, "peak_type": "Voigt"})
+    #     new.param_hints = {"center": {"value": 200}}
+    #     newinst = new()
+    # _newinst_str = "newPeak, <lmfit.Model: Model(voigt, prefix='newPeak_')>, center : -inf < 200 > inf"
+    # self.assertEqual(str(newinst), _newinst_str)
 
 
 # %%
--- tests/deconvolution_models/test_peak_validation.py
+++ tests/deconvolution_models/test_peak_validation.py
@@ -16,20 +16,7 @@ class TestPeakModelValidator(unittest.TestCase):
     def setUp(self):
         self.pmv = PeakModelValidator()
 
-    def test_pmv_valid_models(self):
-        self.assertTrue(self.pmv.valid_models)
-
-    def test_pmv_set_debug(self):
-        self.assertFalse(self.pmv.debug)
-        self.assertTrue(self.pmv._set_debug(**{"debug": True}))
-
-    def test_get_subclasses_from_base(self):
-        with self.assertWarns(NotFoundAnyModelsWarning):
-            self.pmv.get_subclasses_from_base("")
-
-        with self.assertWarns(NotFoundAnyModelsWarning):
-            self.pmv.get_subclasses_from_base(str)
-
+    @unittest.skip("validation is built in to the model")
     def test_validation_inspect_models(self):
         _valid = self.pmv.validation_inspect_models([str])
         self.assertTrue(_valid)
@@ -41,6 +28,7 @@ class TestPeakModelValidator(unittest.TestCase):
         self.assertFalse(_valid[1][0].valid)
         self.assertIn("Unable to initialize model", _valid[1][0].message)
 
+    @unittest.skip("not yet implemented")
     def test_get_cmap_list(self):
         _cmap = self.pmv.get_cmap_list([], cmap_options=())
         self.assertEqual(_cmap, [])
@@ -50,30 +38,6 @@ class TestPeakModelValidator(unittest.TestCase):
         self.assertEqual(_cmap, [self.pmv.fallback_color] * 50)
         _cmap = self.pmv.get_cmap_list([1] * 5)
         self.assertEqual(len(_cmap), 5)
-
-    def test___getattr__(self):
-        with self.assertRaises(AttributeError):
-            self.pmv.fake_attr
-
-    def test___iter__(self):
-        _iter = [i for i in self.pmv]
-        self.assertIsInstance(_iter, list)
-
-    def test_if_lmfit_models(self):
-        if self.pmv.lmfit_models:
-            _getdict = self.pmv.get_dict()
-            self.assertIsInstance(_getdict, dict)
-
-            _getdict = self.pmv.get_model_dict(self.pmv.lmfit_models)
-            self.assertIsInstance(_getdict, dict)
-
-
-def _debugging():
-    self = TestPeakModelValidator()
-    peaks = PeakModelValidator()
-    self.pmv = peaks
-    # all([isinstance(i.peak_model, Model) for i in peaks.lmfit_models])
-    # all([isinstance(i.peak_model, Model) for i in peaks.get_dict().values()])
 
 
 if __name__ == "__main__":
