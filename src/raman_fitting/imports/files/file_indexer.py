fc41fcc8f72b5a116f306c808a0e12eebf2b84cf
David Wallace <wdz.wallace@protonmail.com>
2024-02-11 18:25:15 +0100

    chore: refactor files index functions

--- src/raman_fitting/imports/files/file_indexer.py
+++ src/raman_fitting/imports/files/file_indexer.py
@@ -1,98 +1,202 @@
 """ Indexer for raman data files """
-import logging
+from itertools import groupby, filterfalse
+from typing import List, Sequence
 from pathlib import Path
 
-import pandas as pd
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel,
+    FilePath,
+    NewPath,
+    Field,
+    ValidationError,
+    model_validator,
+    ConfigDict,
+)
 
-from raman_fitting.config.filepath_helper import get_directory_paths_for_run_mode
+from raman_fitting.config import settings
 from raman_fitting.imports.collector import collect_raman_file_infos
+from raman_fitting.imports.models import RamanFileInfo
 
-from .file_finder import find_files
-from .index_funcs import load_index, index_selection
-
-logger = logging.getLogger(__name__)
-logger.propagate = False
+from .utils import load_dataset_from_file
 
 
-class RamanFile(BaseModel):
-    pass
+from loguru import logger
+from tablib import Dataset
 
 
-class MakeRamanFilesIndex:
-    """
+class RamanFileIndex(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
-    Finds the RAMAN files in the data folder from config and creates an overview, on the attribute .index
-    finds a list of files,
+    source: NewPath | FilePath = Field(None, validate_default=False)
+    raman_files: Sequence[RamanFileInfo] = Field(None)
+    dataset: Dataset = Field(None)
+    force_reload: bool = Field(True, validate_default=False)
 
-    """
+    @model_validator(mode="after")
+    def read_or_load_data(self) -> "RamanFileIndex":
+        if not any([self.source, self.raman_files, self.dataset]):
+            raise ValueError("Not all fields should be empty.")
 
-    debug = False
+        if self.source is not None:
+            if self.source.exists() and not self.force_reload:
+                self.dataset = load_dataset_from_file(self.source)
+                self.raman_files = parse_dataset_to_index(self.dataset)
+                return self
+            elif not self.source.exists() and self.force_reload:
+                raise ValidationError(
+                    "Index source file does not exists but was asked to reload from it."
+                )
+            elif not self.source.exists() and not self.force_reload:
+                pass
+            elif self.source.exists() and self.force_reload:
+                logger.info(
+                    f"Index source file {self.source} exists and will be overwritten."
+                )
+        else:
+            logger.debug("Index source file not provided.")
 
-    table_name = "ramanfiles"
-
-    def __init__(
-        self, force_reload=True, run_mode="normal", dataset_dirs=None, **kwargs
-    ):
-        self._cqnm = self.__class__.__qualname__
-
-        self._kwargs = kwargs
-        self.force_reload = force_reload
-        self.run_mode = run_mode
-
-        if not dataset_dirs:
-            dataset_dirs = get_directory_paths_for_run_mode(run_mode=self.run_mode)
-
-        self.dataset_dirs = dataset_dirs
-        for k, val in self.dataset_dirs.items():
-            if isinstance(val, Path):
-                setattr(self, k, val)
-
-        self.raman_files = find_files(self.DATASET_DIR, suffixes=[".txt", ".csv"])
-        self.index_file = dataset_dirs["INDEX_FILE"]
-        self.index = pd.DataFrame()
-        self._error_parse_filenames = []
-        # if "normal" in run_mode and not self.debug and not self.force_reload:
-        index = load_index(self.index_file, reload=self.force_reload)
-        # breakpoint()
-        if index is None:
-            index = collect_raman_file_infos(self.raman_files, **self._kwargs)
-        self.index = index
-        self.index_selection = index_selection(self.index, **self._kwargs)
-
-    def __repr__(self):
-        if self.index is None:
-            return f"{self._cqnm} with index (0)"
-        return f"{self._cqnm} with index ({len(self.index)})"
-
-    def __len__(self):
-        if self.index is None:
-            return 0
-        return len(self.index)
+        if self.raman_files is not None:
+            if self.dataset is None:
+                self.dataset = cast_raman_files_to_dataset(self.raman_files)
+            else:
+                dataset_rf = cast_raman_files_to_dataset(self.raman_files)
+                assert (
+                    dataset_rf == self.dataset
+                ), "Both dataset and raman_files provider but are different."
+        if self.dataset is not None:
+            self.raman_files = parse_dataset_to_index(self.dataset)
+        elif self.dataset is None and self.raman_files is None:
+            raise ValueError(
+                "Index error, both raman_files and dataset are not provided."
+            )
 
 
-def create_raman_file_index(raman_files, **kwargs):
+def cast_raman_files_to_dataset(raman_files: List[RamanFileInfo]) -> Dataset:
+    headers = list(RamanFileInfo.model_fields.keys())
+    data = Dataset(headers=headers)
+    for file in raman_files:
+        data.append(file.model_dump(mode="json").values())
+    return data
+
+
+def parse_dataset_to_index(dataset: Dataset) -> List[RamanFileInfo]:
+    raman_file = []
+    for row in dataset:
+        row_data = dict(zip(dataset.headers, row))
+        raman_file.append(RamanFileInfo(**row_data))
+    return raman_file
+
+
+class IndexSelector(BaseModel):
+    raman_files: Sequence[RamanFileInfo]
+    sample_IDs: List[str] = Field(default_factory=list)
+    sample_groups: List[str] = Field(default_factory=list)
+    selection: Sequence[RamanFileInfo] = Field(default_factory=list)
+
+    @model_validator(mode="after")
+    def make_and_set_selection(self) -> "IndexSelector":
+        rf_index = self.raman_files
+        if not any([self.sample_groups, self.sample_IDs]):
+            self.selection = rf_index
+            logger.debug(
+                f"{self.__class__.__qualname__} selected {len(self.selection)} of {len(rf_index)}. "
+            )
+            return self
+        else:
+            rf_index_groups = list(
+                filter(lambda x: x.sample.group in self.sample_groups, rf_index)
+            )
+            _pre_selected_samples = {i.sample.id for i in rf_index_groups}
+            selected_sampleIDs = filterfalse(
+                lambda x: x in _pre_selected_samples, self.sample_IDs
+            )
+            rf_index_samples = list(
+                filter(lambda x: x.sample.id in selected_sampleIDs, rf_index)
+            )
+            rf_selection_index = rf_index_groups + rf_index_samples
+            self.selection = rf_selection_index
+            logger.debug(
+                f"{self.__class__.__qualname__} selected {len(self.selection)} of {rf_index}. "
+            )
+            return self
+
+
+def groupby_sample_group(index: List[RamanFileInfo]):
+    """Generator for Sample Groups, yields the name of group and group of the index SampleGroup"""
+    grouper = groupby(index, key=lambda x: x.sample.group)
+    return grouper
+
+
+def groupby_sample_id(index: List[RamanFileInfo]):
+    """Generator for SampleIDs, yields the name of group, name of SampleID and group of the index of the SampleID"""
+    grouper = groupby(index, key=lambda x: x.sample.id)
+    return grouper
+
+
+def iterate_over_groups_and_sample_id(index: List[RamanFileInfo]):
+    for grp_name, grp in groupby_sample_group(index):
+        for sample_id, sgrp in groupby_sample_group(grp):
+            yield grp_name, grp, sample_id, sgrp
+
+
+def select_index_by_sample_groups(index: List[RamanFileInfo], sample_groups: List[str]):
+    return filter(lambda x: x.sample.group in sample_groups, index)
+
+
+def select_index_by_sample_ids(index: List[RamanFileInfo], sample_ids: List[str]):
+    return filter(lambda x: x.sample.id in sample_ids, index)
+
+
+def select_index(
+    index: List[RamanFileInfo], sample_groups: List[str], sample_ids: List[str]
+):
+    group_selection = list(select_index_by_sample_groups(index, sample_groups))
+    sample_selection = list(select_index_by_sample_ids(index, sample_ids))
+    selection = group_selection + sample_selection
+    return selection
+
+
+def collect_raman_file_index_info(
+    raman_files: Sequence[Path] | None = None, **kwargs
+) -> List[RamanFileInfo]:
     """loops over the files and scrapes the index data from each file"""
-
-    index = collect_raman_file_infos(raman_files, kwargs)
-    # breakpoint()
-    # index = pd.DataFrame([i.parse_result for i in pp_collection])
-    # index = _extra_assign_destdir_and_set_paths(index)
+    if not raman_files:
+        raman_files = list(settings.internal_paths.example_fixtures.glob("*.txt"))
+    index = collect_raman_file_infos(raman_files, **kwargs)
     logger.info(f"successfully made index {len(index)} from {len(raman_files)} files")
-    if 0:
-        logger.info(f"errors for filename parser from {len(raman_files)} files")
     return index
+
+
+def initialize_index(files: Sequence[Path] | None = None, force_reload: bool = False):
+    index_file = settings.destination_dir.joinpath("index.csv")
+    raman_files = collect_raman_file_index_info(raman_files=files)
+    index_data = {
+        "file": index_file,
+        "raman_files": raman_files,
+        "force_reload": force_reload,
+    }
+    raman_index = RamanFileIndex(**index_data)
+    logger.info(
+        f"index_delegator index prepared with len {len(raman_index.raman_files)}"
+    )
+    return raman_index
 
 
 def main():
     """test run for indexer"""
-    RamanIndex = None
+    index_file = settings.destination_dir.joinpath("index.csv")
+    raman_files = collect_raman_file_index_info()
     try:
-        RamanIndex = MakeRamanFilesIndex(read_data=True, run_mode="make_examples")
-
+        index_data = {"file": index_file, "raman_files": raman_files}
+        raman_index = RamanFileIndex(**index_data)
+        logger.debug(f"Raman Index len: {len(raman_index.dataset)}")
     except Exception as e:
         logger.error(f"Raman Index error: {e}")
-    return RamanIndex
+    # breakpoint()
+    select_index(raman_index.raman_files, sample_groups=["DW"], sample_ids=["DW38"])
+    # ds = cast_raman_files_to_dataset(raman_index.raman_files)
+
+    return raman_index
 
 
 if __name__ == "__main__":
