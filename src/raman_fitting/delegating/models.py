5ae1ef05d4456c5533740c796522330ade608e33
David Wallace <wdz.wallace@protonmail.com>
2024-02-11 18:19:28 +0100

    chore: update delegating functions

---
+++ src/raman_fitting/delegating/models.py
@@ -0,0 +1,29 @@
+# pylint: disable=W0614,W0401,W0611,W0622,C0103,E0401,E0402
+from typing import Dict, Sequence
+
+from pydantic import BaseModel
+
+from raman_fitting.imports.models import RamanFileInfo
+
+from raman_fitting.models.spectrum import SpectrumData
+from raman_fitting.models.fit_models import SpectrumFitModel
+from raman_fitting.models.splitter import WindowNames
+from raman_fitting.imports.spectrumdata_parser import SpectrumReader
+from raman_fitting.processing.post_processing import SpectrumProcessor
+
+
+class PreparedSampleSpectrum(BaseModel):
+    file_info: RamanFileInfo
+    read: SpectrumReader
+    processed: SpectrumProcessor
+
+
+class AggregatedSampleSpectrum(BaseModel):
+    sources: Sequence[PreparedSampleSpectrum]
+    spectrum: SpectrumData
+
+
+class AggregatedSampleSpectrumFitResult(BaseModel):
+    window_name: WindowNames
+    aggregated_spectrum: AggregatedSampleSpectrum
+    fit_model_results: Dict[str, SpectrumFitModel]
