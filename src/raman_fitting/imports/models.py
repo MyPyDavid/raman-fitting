d0c4522e4d07ade303ef4897c48147afa451d820
David Wallace <wdz.wallace@protonmail.com>
2024-02-17 14:35:53 +0100

    refactor and rename funcs and vars

--- src/raman_fitting/imports/models.py
+++ src/raman_fitting/imports/models.py
@@ -6,7 +6,7 @@ from pydantic import (
     ConfigDict,
 )
 
-from .samples.sample_id_helpers import parse_sample_from_filepath
+from .samples.sample_id_helpers import extract_sample_metadata_from_filepath
 
 from .files.metadata import FileMetaData, get_file_metadata
 from .files.index_helpers import get_filename_id_from_path
@@ -29,7 +29,7 @@ class RamanFileInfo(BaseModel):
 
     @model_validator(mode="after")
     def parse_and_set_sample_from_file(self) -> "RamanFileInfo":
-        sample = parse_sample_from_filepath(self.file)
+        sample = extract_sample_metadata_from_filepath(self.file)
         self.sample = sample
         return self
 
@@ -38,13 +38,3 @@ class RamanFileInfo(BaseModel):
         file_metadata = get_file_metadata(self.file)
         self.file_metadata = FileMetaData(**file_metadata)
         return self
-
-
-# def extra_assign_export_dir_on_index(result_dir, index: List[RamanFileInfo]):
-#     """assign the DestDir column to index and sets column values as object type"""
-#     _index = []
-
-#     for rf_info in index:
-#         rf_info.export_dir = result_dir.joinpath(rf_info.sample.group)
-#         _index.append(rf_info)
-#     return _index
