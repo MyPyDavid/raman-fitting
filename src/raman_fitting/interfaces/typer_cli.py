aadf5fd92aa5567f6213b7e0ff513126ae9a8a48
David Wallace <mypydavid@proton.me>
2024-03-09 12:25:41 +0100

    cli: fix and add make cmd

--- src/raman_fitting/interfaces/typer_cli.py
+++ src/raman_fitting/interfaces/typer_cli.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 from typing import List, Optional
 from typing_extensions import Annotated
 
-
+from pathlib import Path
 from enum import StrEnum, auto
 from loguru import logger
 from raman_fitting.config.path_settings import RunModes
 from raman_fitting.delegating.main_delegator import MainDelegator
+from raman_fitting.imports.files.file_indexer import initialize_index_from_source_files
 from .utils import get_package_version
 
 import typer
@@ -16,6 +15,7 @@ import typer
 class MakeTypes(StrEnum):
     INDEX = auto()
     CONFIG = auto()
+    EXAMPLE = auto()
 
 
 __version__ = "0.1.0"
@@ -64,24 +64,31 @@ def run(
     ],
     run_mode: Annotated[RunModes, typer.Argument()] = RunModes.NORMAL,
 ):
+    if run_mode is None:
+        print("No make run mode passed")
+        raise typer.Exit()
     kwargs = {"run_mode": run_mode}
-    if run_mode == RunModes.NORMAL:
-        pass
-        # _org_index = OrganizeRamanFiles()
-        # RL = RamanLoop(_org_index, run_mode ='normal')
-    elif run_mode.lower() == RunModes.DEBUG:
-        pass
-        # IDEA Add a FAST TRACK for DEBUG
-    elif run_mode == RunModes.EXAMPLES:
+    if run_mode == RunModes.EXAMPLES:
         kwargs.update({"fit_model_specific_names": ["2peaks", "3peaks", "4peaks"]})
     logger.info(f"Starting raman_fitting with CLI args:\n{run_mode}")
     _main_run = MainDelegator(**kwargs)
 
 
 @app.command()
-def make(make_type: Annotated[MakeTypes, typer.Argument()]):
+def make(
+    source_files: Annotated[List[Path], typer.Option()],
+    index_file: Annotated[List[Path], typer.Option()],
+    force_reindex: Annotated[bool, typer.Option("--force-reindex")] = False,
+    make_type: Annotated[MakeTypes, typer.Option()] = None,
+):
+    if make_type is None:
+        print("No make type args passed")
+        raise typer.Exit()
+
     if make_type == MakeTypes.INDEX:
-        pass  # make index
+        initialize_index_from_source_files(
+            files=source_files, index_file=index_file, force_reindex=force_reindex
+        )
 
     elif make_type == MakeTypes.CONFIG:
         pass  # make config
