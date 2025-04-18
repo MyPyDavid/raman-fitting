78ac8625cf76cd0b72a2ad107ba72c182ae0992d
David Wallace <mypydavid@proton.me>
2025-04-18 12:19:46 +0200

    style: fix compat with >=311
    Signed-off-by: David Wallace <mypydavid@proton.me>

--- src/raman_fitting/utils/compat.py
+++ src/raman_fitting/utils/compat.py
@@ -1,7 +1,7 @@
 import sys
-from enum import Enum
 
 if sys.version_info < (3, 11):
+    from enum import Enum
 
     class StrEnum(str, Enum):
         """Custom implementation of StrEnum for Python <3.11."""
@@ -9,4 +9,4 @@ if sys.version_info < (3, 11):
         def _generate_next_value_(name, start, count, last_values):
             return name.lower()  # Automatically assign lowercase names as values
 else:
-    pass
+    from enum import StrEnum  # noqa: F401
